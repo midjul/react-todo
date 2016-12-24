2e009bfd503ae9164e6baa0eb2304f5a2d6314d9
Midhet Đulović <djulovic_m@hotmail.com>
2016-12-24 23:40:12 +0100

    Add Search component

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,9 +1,14 @@
 var React=require('react');
 var TodoList=require('TodoList');
 var AddTodo=require('AddTodo');
+var TodoSearch=require('TodoSearch');
+
+
 var TodoApp=React.createClass({
   getInitialState:function () {
    return {
+     showCompleted:false,
+     searchText:'',
      todos:[
      {id:1,
        text:'Walk the dog'
@@ -22,6 +27,12 @@ var TodoApp=React.createClass({
      ]
    };
   },
+  handleSearch:function (showCompleted, searchText) {
+    this.setState({
+     showCompleted:showCompleted,
+     searchText:searchText.toLowerCase()
+    });
+  },
 
   handleAddTo:function (text) {
 
@@ -32,6 +43,7 @@ render:function () {
 
   return (
      <div>
+       <TodoSearch onSearch={this.handleSearch} />
     <TodoList todos={todos} />
     <AddTodo onAddTodo={this.handleAddTo}/>
      </div>
--- public/bundle.js
+++ public/bundle.js
@@ -109,7 +109,7 @@
 	$(document).foundation();
 
 	// App css
-	__webpack_require__(233);
+	__webpack_require__(234);
 
 	ReactDOM.render(React.createElement(TodoApp, null), document.getElementById('app'));
 	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(7)))
@@ -25475,11 +25475,15 @@
 	var React = __webpack_require__(8);
 	var TodoList = __webpack_require__(230);
 	var AddTodo = __webpack_require__(232);
+	var TodoSearch = __webpack_require__(233);
+
 	var TodoApp = React.createClass({
 	  displayName: 'TodoApp',
 
 	  getInitialState: function getInitialState() {
 	    return {
+	      showCompleted: false,
+	      searchText: '',
 	      todos: [{ id: 1,
 	        text: 'Walk the dog'
 	      }, {
@@ -25493,6 +25497,12 @@
 	      }]
 	    };
 	  },
+	  handleSearch: function handleSearch(showCompleted, searchText) {
+	    this.setState({
+	      showCompleted: showCompleted,
+	      searchText: searchText.toLowerCase()
+	    });
+	  },
 
 	  handleAddTo: function handleAddTo(text) {
 
@@ -25505,6 +25515,7 @@
 	    return React.createElement(
 	      'div',
 	      null,
+	      React.createElement(TodoSearch, { onSearch: this.handleSearch }),
 	      React.createElement(TodoList, { todos: todos }),
 	      React.createElement(AddTodo, { onAddTodo: this.handleAddTo })
 	    );
@@ -25620,13 +25631,55 @@
 /* 233 */
 /***/ function(module, exports, __webpack_require__) {
 
+	"use strict";
+
+	var React = __webpack_require__(8);
+
+	var TodoSearch = React.createClass({
+	  displayName: "TodoSearch",
+
+	  handleSearch: function handleSearch() {
+	    var showCompleted = this.refs.showCompleted.checked;
+	    var searchText = this.refs.searchText.value;
+
+	    this.props.onSearch(showCompleted, searchText);
+	  },
+	  render: function render() {
+	    return React.createElement(
+	      "div",
+	      null,
+	      React.createElement(
+	        "div",
+	        null,
+	        React.createElement("input", { type: "search", ref: "searchText", placeholder: "Search todos", onChange: this.handleSearch })
+	      ),
+	      React.createElement(
+	        "div",
+	        null,
+	        React.createElement(
+	          "label",
+	          null,
+	          React.createElement("input", { type: "checkbox", ref: "showCompleted", onChange: this.handleSearch }),
+	          "Sow completed todos"
+	        )
+	      )
+	    );
+	  }
+	});
+
+	module.exports = TodoSearch;
+
+/***/ },
+/* 234 */
+/***/ function(module, exports, __webpack_require__) {
+
 	// style-loader: Adds some css to the DOM by adding a <style> tag
 
 	// load the styles
-	var content = __webpack_require__(234);
+	var content = __webpack_require__(235);
 	if(typeof content === 'string') content = [[module.id, content, '']];
 	// add the styles to the DOM
-	var update = __webpack_require__(236)(content, {});
+	var update = __webpack_require__(237)(content, {});
 	if(content.locals) module.exports = content.locals;
 	// Hot Module Replacement
 	if(false) {
@@ -25643,10 +25696,10 @@
 	}
 
 /***/ },
-/* 234 */
+/* 235 */
 /***/ function(module, exports, __webpack_require__) {
 
-	exports = module.exports = __webpack_require__(235)();
+	exports = module.exports = __webpack_require__(236)();
 	// imports
 
 
@@ -25657,7 +25710,7 @@
 
 
 /***/ },
-/* 235 */
+/* 236 */
 /***/ function(module, exports) {
 
 	/*
@@ -25713,7 +25766,7 @@
 
 
 /***/ },
-/* 236 */
+/* 237 */
 /***/ function(module, exports, __webpack_require__) {
 
 	/*
