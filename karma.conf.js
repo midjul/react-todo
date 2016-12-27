90c348163137299f987124244a1a55386374205d
Midhet Đulović <djulovic_m@hotmail.com>
2016-12-27 20:29:43 +0100

    Style todo application

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,8 @@ var webpackConfig=require('./webpack.config.js');
 
 module.exports=function(config){
  config.set({
-  browsers:['Firefox'],
+  //browsers:['Firefox'],
+  browsers:['Chrome'],
    singleRun: true,
    frameworks:['mocha'],
    files:[
