fd6bacf2493501975dfad400535cc56f1fb60926
Midhet Đulović <djulovic_m@hotmail.com>
2017-01-30 18:55:35 +0100

    Add actions & action tests

---
+++ app/tests/actions/actions.test.jsx
@@ -0,0 +1,46 @@
+var expect=require('expect');
+var actions=require('actions');
+
+describe('Actions', ()=>{
+it('shoud generate search text action', ()=>{
+   var action={
+     type:'SET_SEARCH_TEXT',
+     searchText:'test text'
+   };
+   var res=actions.setSearchText('test text');
+   expect(res).toEqual(action);
+});
+
+it('shoud generate add to do action', ()=>{
+var action={
+  type:'ADD_TODO',
+  text:'test text'
+};
+
+var res=actions.addTodo('test text');
+
+expect(res).toEqual(action);
+
+});
+
+it('shoud generate toggleShowCompleted action',()=>{
+var action={
+  type:'TOGGLE_SHOW_COMPLETED'
+};
+     var res=actions.toggleShowCompleted();
+
+     expect(res).toEqual(action);
+});
+
+it('shoud generate toggletodo action', ()=>{
+  var action={
+    type:'TOGGLE_TODO',
+    id:123
+  };
+
+  var res=actions.toggleTodo(action.id);
+
+  expect(res).toEqual(action);
+});
+
+});
